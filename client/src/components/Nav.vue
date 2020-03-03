417f95007bc33ec08dfd8713363c95ee237d762a
Mike Parish <michaeljparish@gmail.com>
2020-03-03 18:13:49 -0500

    added user profile creation w/2-way data binding

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -3,14 +3,13 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
         
         <div class="navbar-brand">
-            <a class="navbar-item" href="http://localhost:8080">
-            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-            </a>
-
+		    <a class="navbar-item" href="http://localhost:8080/user">
+			    <h1 class="has-text-weight-bold">FitnessTracker</h1>
+		    </a>
             <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
             </a>
         </div>
 
@@ -18,43 +17,34 @@
         
         <div class="navbar-start">
         
-        <router-link to="/" class="navbar-item">Home</router-link>
+        <!-- <router-link to="/" class="navbar-item">Home</router-link> -->
         <router-link to="/about" class="navbar-item">About</router-link>
+        <router-link to="/user" class="navbar-item">User</router-link>
 
-        <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link">
-            More
-            </a>
+        <!-- More nav dropdown
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                More
+                </a>
 
-            <div class="navbar-dropdown">
-            <a class="navbar-item">
-                About
-            </a>
-            <a class="navbar-item">
-                Jobs
-            </a>
-            <a class="navbar-item">
-                Contact
-            </a>
-            <hr class="navbar-divider">
-            <a class="navbar-item">
-                Report an issue
-            </a>
-            </div>
-        </div>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">About</a>
+                    <a class="navbar-item">Jobs</a>
+                    <a class="navbar-item">Contact</a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item">Report an issue</a>
+                </div>
+            </div>-->
+        
         </div>
 
         <div class="navbar-end">
-        <div class="navbar-item">
-            <div class="buttons">
-            <a class="button is-primary">
-                <strong>Sign up</strong>
-            </a>
-            <a class="button is-light">
-                Log in
-            </a>
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-success" href="http://localhost:8080/user"><strong>Sign up</strong></a>
+                    <a class="button is-light">Log in</a>
+                </div>
             </div>
-        </div>
         </div>
     </div>
     </nav>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   
   <div class="home">
-  
+    <h1>this is a test on home.vue</h1>
   </div>
 
 </template>
